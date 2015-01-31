82bdd4933d1e32ed48855d1118c0b6c765c5f462
Jeffrey Smith <jeffrey.smith@hotmail.co.uk>
2015-01-31 11:32:27 +0000

    Added soundex from rosetta code and an example to the README.md

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 gofuzzy
 =======
 
-Example
+Golang Fuzzy matching Library
+
+This is just a testing repo to learn how to use github properly.
+
+
+Levenshtein distance Example
 -------
 
 	package main
@@ -17,5 +22,28 @@ Example
 		fmt.Println(gofuzzy.Ld("Miles","Apart"))
 	}
 
+Soundex Example
+-------
 
-Golang Fuzzy matching Library
+	package main
+	
+	import (
+		"fmt"
+		"github.com/jeffsmith82/gofuzzy"
+	)
+
+	func main(){
+		fmt.Println(gofuzzy.Soundex("Robert"))
+		fmt.Println(gofuzzy.Soundex("Rupert"))
+		fmt.Println(gofuzzy.Soundex("Rubin"))
+		fmt.Println(gofuzzy.Soundex("ashcroft"))
+		fmt.Println(gofuzzy.Soundex("ashcraft"))
+		fmt.Println(gofuzzy.Soundex("moses"))
+		fmt.Println(gofuzzy.Soundex("O'Mally"))
+		fmt.Println(gofuzzy.Soundex("d jay"))
+		fmt.Println(gofuzzy.Soundex("R2-D2"))
+		fmt.Println(gofuzzy.Soundex("12p2"))
+		fmt.Println(gofuzzy.Soundex("naïve"))
+		fmt.Println(gofuzzy.Soundex(""))
+		fmt.Println(gofuzzy.Soundex("bump\t"))
+	}
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package gofuzzy
 
+import (
+	"errors"
+	"unicode"
+)
+
+var code = []byte("01230127022455012623017202")
+
 func Ld(s, t string) int {
 	d := make([][]int, len(s)+1)
 	for i := range d {
@@ -29,4 +36,54 @@ func Ld(s, t string) int {
 
 	}
 	return d[len(s)][len(t)]
+}
+
+func Soundex(s string) (string, error) {
+	var sx [4]byte
+	var sxi int
+	var cx, lastCode byte
+	for i, c := range s {
+		switch {
+		case !unicode.IsLetter(c):
+			if c < ' ' || c == 127 {
+				return "", errors.New("ASCII control characters disallowed")
+			}
+			if i == 0 {
+				return "", errors.New("initial character must be a letter")
+			}
+			lastCode = '0'
+			continue
+		case c >= 'A' && c <= 'Z':
+			cx = byte(c - 'A')
+		case c >= 'a' && c <= 'z':
+			cx = byte(c - 'a')
+		default:
+			return "", errors.New("non-ASCII letters unsupported")
+		}
+		// cx is valid letter index at this point
+		if i == 0 {
+			sx[0] = cx + 'A'
+			sxi = 1
+			continue
+		}
+		switch x := code[cx]; x {
+		case '7', lastCode:
+		case '0':
+			lastCode = '0'
+		default:
+			sx[sxi] = x
+			if sxi == 3 {
+				return string(sx[:]), nil
+			}
+			sxi++
+			lastCode = x
+		}
+	}
+	if sxi == 0 {
+		return "", errors.New("no letters present")
+	}
+	for ; sxi < 4; sxi++ {
+		sx[sxi] = '0'
+	}
+	return string(sx[:]), nil
 }
